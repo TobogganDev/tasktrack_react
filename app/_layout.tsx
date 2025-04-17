ae33b23f341a08cb53052fe3184668dbc3a6821a
unknown <moureau.marc78@gmail.com>
2025-04-17 09:22:18 +0200

    :sparkles: Advanced management of Supabase and New font :sparkles:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,27 @@
-import React from "react";
-import { Slot } from "expo-router";
-import { AuthProvider } from "@/context/AuthContext";
+import React from 'react';
+import { Montserrat_100Thin, Montserrat_400Regular, Montserrat_600SemiBold, useFonts } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect as reactUseEffect } from 'react';
 
-export default function RootLayout() {
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+    const [loaded, error] = useFonts({
+        Montserrat_100Thin,
+        Montserrat_400Regular,
+        Montserrat_600SemiBold,
+    });
+
+    reactUseEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+
     return (
         <AuthProvider>
             <Slot />
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,19 +1,31 @@
-import "react-native-url-polyfill/auto";
+import { Platform } from "react-native";
+
+if (Platform.OS !== "web") {
+    require("react-native-url-polyfill/auto");
+}
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-let supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-let supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL or Anon Key is not defined in environment variables.");
 }
 
+const isWeb = Platform.OS === "web";
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        storage: AsyncStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: false,
-    },
+    auth: isWeb
+        ? {
+              storage: localStorage,
+              detectSessionInUrl: true,
+          }
+        : {
+              storage: AsyncStorage,
+              autoRefreshToken: true,
+              persistSession: true,
+              detectSessionInUrl: false,
+          },
 });
